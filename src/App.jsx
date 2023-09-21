ff4fb92a6166e8cdd1db26d686d4235d915939ea
Collins Bawa <collinskwamebawa@yahoo.com>
2023-09-21 20:40:37 +0000

    Import free combos component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import PredictionsList from './components/predictions/PredictionsList';
+import FreeCombosList from './components/free_combos/Free_CombosList';
 
 const App = () => (
   <BrowserRouter>
+    <FreeCombosList />
     <Routes>
       <Route path="/" element={<PredictionsList />} />
     </Routes>
