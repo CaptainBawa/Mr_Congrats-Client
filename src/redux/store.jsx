82901d36d14493a0a49c6eb798c1bc5d71ef8bd5
Collins Bawa <collinskwamebawa@yahoo.com>
2023-09-21 20:40:55 +0000

    Import free combos reducer

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import predictionsReducer from './slice/predictionsSlice';
+import freeCombosReducer from './slice/freeCombosSlice';
 
 const store = configureStore({
   reducer: {
+    freeCombos: freeCombosReducer,
     predictions: predictionsReducer,
   },
 });
