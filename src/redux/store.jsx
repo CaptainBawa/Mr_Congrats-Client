b847b9273208b145aa7bef7842d3f749198e2d2e
Collins Bawa <collinskwamebawa@yahoo.com>
2023-09-22 18:44:09 +0000

    Import paid combos reducer

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import predictionsReducer from './slice/predictionsSlice';
 import freeCombosReducer from './slice/freeCombosSlice';
+import paidCombosReducer from './slice/paidCombosSlice';
 
 const store = configureStore({
   reducer: {
+    paidCombos: paidCombosReducer,
     freeCombos: freeCombosReducer,
     predictions: predictionsReducer,
   },
