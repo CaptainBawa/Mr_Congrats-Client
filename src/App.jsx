a57fb461afda4042ebed3cec87846ed02525c486
Collins Bawa <collinskwamebawa@yahoo.com>
2023-09-22 18:43:42 +0000

    Import paid combos component

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import PredictionsList from './components/predictions/PredictionsList';
 import FreeCombosList from './components/free_combos/Free_CombosList';
+import PaidCombosList from './components/paid_combos/Paid_CombosList';
 
 const App = () => (
   <BrowserRouter>
+    <PaidCombosList />
     <FreeCombosList />
     <Routes>
       <Route path="/" element={<PredictionsList />} />
