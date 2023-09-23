7bb9a6f20f9b912812e77205a9cd1eccd7f05df9
Collins Bawa <collinskwamebawa@yahoo.com>
2023-09-23 15:43:01 +0000

    Import users reducer

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import predictionsReducer from './slice/predictionsSlice';
 import freeCombosReducer from './slice/freeCombosSlice';
 import paidCombosReducer from './slice/paidCombosSlice';
+import usersReducer from './slice/usersSlice';
 
 const store = configureStore({
   reducer: {
     paidCombos: paidCombosReducer,
     freeCombos: freeCombosReducer,
     predictions: predictionsReducer,
+    users: usersReducer,
   },
 });
 export default store;
