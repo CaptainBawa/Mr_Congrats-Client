cc08b7b19df2abc6a34e153fa6b954c3769d1fab
Collins Bawa <collinskwamebawa@yahoo.com>
2023-09-23 18:44:16 +0000

    Import advert and details reducers

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -3,9 +3,13 @@ import predictionsReducer from './slice/predictionsSlice';
 import freeCombosReducer from './slice/freeCombosSlice';
 import paidCombosReducer from './slice/paidCombosSlice';
 import usersReducer from './slice/usersSlice';
+import advertsReducer from './slice/advertsSlice';
+import detailsReducer from './slice/detailsSlice';
 
 const store = configureStore({
   reducer: {
+    details: detailsReducer,
+    adverts: advertsReducer,
     paidCombos: paidCombosReducer,
     freeCombos: freeCombosReducer,
     predictions: predictionsReducer,
