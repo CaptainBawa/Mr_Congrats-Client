ccbc6aee2dd16bbe94d76dea4346396148e5998d
Collins Bawa <collinskwamebawa@yahoo.com>
2023-09-23 18:45:22 +0000

    Display details on the page

---
+++ src/components/details/DetailsList.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchDetails, selectDetails } from '../../redux/slice/detailsSlice';
+
+const DetailsList = () => {
+  const dispatch = useDispatch();
+  const details = useSelector(selectDetails);
+
+  useEffect(() => {
+    dispatch(fetchDetails());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Home Team</th>
+            <th>Away Team</th>
+            <th>League</th>
+            <th>Result</th>
+            <th>Text</th>
+          </tr>
+        </thead>
+        <tbody>
+          {details.map((detail) => (
+            <tr key={detail.id}>
+              <td>{detail.date}</td>
+              <td>{detail.home_team}</td>
+              <td>{detail.away_team}</td>
+              <td>{detail.league}</td>
+              <td>{detail.result}</td>
+              <td>{detail.text}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default DetailsList;
