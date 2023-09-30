30564b3196d18bec9ad3ff52feb2e925482ba6aa
Collins Bawa <collinskwamebawa@yahoo.com>
2023-09-30 00:46:29 +0000

    Add bookies component

---
+++ src/components/Bookies.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Bookies = () => (
+  <div className="bookies-container">
+    <h2>Enjoy the Best Betting Offers and Free Tips 24/7!</h2>
+    <ul>
+      <li><a href="null">Bonus</a></li>
+      <li><a href="null">Promo code</a></li>
+      <li><a href="null">Free bets</a></li>
+      <li className="big"><a href="null">Big odds bookies (stake small, win big)</a></li>
+    </ul>
+  </div>
+);
+
+export default Bookies;
