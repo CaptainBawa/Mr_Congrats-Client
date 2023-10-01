1e909fa845568b93a2e49f8633b4277e4a69e07c
Collins Bawa <collinskwamebawa@yahoo.com>
2023-10-01 18:54:43 +0000

    Add social icons

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,26 @@ import React from 'react';
 const Footer = () => (
   <div>
     <ul>
-      <li><a href="null">testing</a></li>
+      <li>
+        <a href="tel:YOUR_PHONE_NUMBER_HERE">
+          <i className="fas fa-phone" />
+        </a>
+      </li>
+      <li>
+        <a href="YOUR_TELEGRAM_LINK_HERE" target="_blank" rel="noreferrer">
+          <i className="fab fa-telegram" />
+        </a>
+      </li>
+      <li>
+        <a href="YOUR_WHATSAPP_LINK_HERE" target="_blank" rel="noreferrer">
+          <i className="fab fa-whatsapp" />
+        </a>
+      </li>
+      <li>
+        <a href="https://twitter.com/BawaCollins" target="_blank" rel="noreferrer">
+          <i className="fab fa-twitter" />
+        </a>
+      </li>
     </ul>
   </div>
 );
