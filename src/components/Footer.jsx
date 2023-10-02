0225db70e9de360c33ea9e664271ed8889044cd1
Collins Bawa <collinskwamebawa@yahoo.com>
2023-10-02 17:14:44 +0000

    Update file name

---
+++ src/components/Footer.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer>
+    <ul>
+      <li>
+        <a href="tel:YOUR_PHONE_NUMBER_HERE">
+          <i className="fas fa-phone" />
+        </a>
+      </li>
+      <li>
+        <a href="YOUR_TELEGRAM_LINK_HERE" target="_blank" rel="noreferrer">
+          <i className="fab fa-telegram" />
+        </a>
+      </li>
+      <li>
+        <a href="YOUR_WHATSAPP_LINK_HERE" target="_blank" rel="noreferrer">
+          <i className="fab fa-whatsapp" />
+        </a>
+      </li>
+      <li>
+        <a href="https://twitter.com/BawaCollins" target="_blank" rel="noreferrer">
+          <i className="fab fa-twitter" />
+        </a>
+      </li>
+    </ul>
+  </footer>
+);
+
+export default Footer;
