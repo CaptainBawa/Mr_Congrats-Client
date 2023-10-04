112332c4c70832f64ea412a223f3a681b40a4cb6
Collins Bawa <collinskwamebawa@yahoo.com>
2023-10-04 17:15:59 +0000

    Add logic to render logout when a user is logged in

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,11 +1,19 @@
 import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout } from '../redux/slice/authSlice';
 import logo from '../assets/mrcongrats.jpg';
 
 const Navigation = () => {
   const [menuVisible, setMenuVisible] = useState(false);
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const dispatch = useDispatch();
 
   const handleToggleMenu = () => {
     setMenuVisible((prevState) => !prevState);
+  };
+
+  const handleLogout = () => {
+    dispatch(logout());
   };
 
   useEffect(() => {
@@ -42,8 +50,16 @@ const Navigation = () => {
         <ul>
           <li className="nav-list-item"><a href="#skill">Home</a></li>
           <li className="nav-list-item"><a href="#work">About Us</a></li>
-          <li className="nav-list-item"><a href="#projects">Login</a></li>
-          <li className="nav-list-item"><a href="#blog">Sign Up</a></li>
+          {isAuthenticated ? (
+            <li className="nav-list-item">
+              <button type="button" onClick={handleLogout}>Logout</button>
+            </li>
+          ) : (
+            <>
+              <li className="nav-list-item"><a href="#projects">Login</a></li>
+              <li className="nav-list-item"><a href="#blog">Sign Up</a></li>
+            </>
+          )}
           <li className="nav-list-item"><a href="#contact">Contact Us</a></li>
         </ul>
       </nav>
