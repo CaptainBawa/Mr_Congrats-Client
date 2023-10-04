5ed2ab4d29fb244b8483fbb2ac345e941c05c51a
Collins Bawa <collinskwamebawa@yahoo.com>
2023-10-04 17:15:09 +0000

    Add login and signup component

---
+++ src/components/auth/Login.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { login } from '../../redux/slice/authSlice';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const dispatch = useDispatch();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('http://localhost:3000/login', { user: { email, password } });
+      window.location.href = '/';
+      const user = response.data;
+      dispatch(login(user));
+    } catch (error) {
+      if (error.response) {
+        const errorData = error.response.data;
+        const errorMessage = errorData.message || 'An error occurred during login.';
+        setErrorMessage(errorMessage);
+      } else if (error.request) {
+        setErrorMessage('No response from the server. Please try again later.');
+      } else {
+        setErrorMessage('An error occurred. Please try again later.');
+      }
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <p>
+        Don&rsquo;t have an account?
+        <Link to="/signup">Sign up</Link>
+      </p>
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
+      <form onSubmit={handleLogin}>
+        <input
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="input-field"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="input-field"
+        />
+        <button type="submit" className="login-button">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/components/auth/Signup.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Signup = () => {
+  const [name, setname] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.post('http://localhost:3000/signup', {
+        user: {
+          name, email, password,
+        },
+      });
+      window.location.href = '/login';
+    } catch (error) {
+      if (error.response) {
+        const errorData = error.response.data;
+        const errorMessage = errorData.message || 'An error occurred during signup.';
+        setErrorMessage(errorMessage);
+      } else if (error.request) {
+        setErrorMessage('No response from the server. Please try again later.');
+      } else {
+        setErrorMessage('An error occurred. Please try again later.');
+      }
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Signup</h2>
+      <p>
+        Already have an account?
+        <Link to="/login">Login</Link>
+      </p>
+      {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+      <form onSubmit={handleSignup}>
+        <input type="text" placeholder="name" value={name} onChange={(e) => setname(e.target.value)} className="input-field" />
+        <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} className="input-field" />
+        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} className="input-field" />
+        <button type="submit" className="login-button">Signup</button>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
