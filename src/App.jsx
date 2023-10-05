2e534d841a69bbdf1a39e402a58304f17ff15707
Collins Bawa <collinskwamebawa@yahoo.com>
2023-10-05 23:20:30 +0000

    Remove free and paid combo component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import PredictionsList from './components/predictions/PredictionsList';
-import FreeCombosList from './components/free_combos/Free_CombosList';
-import PaidCombosList from './components/paid_combos/Paid_CombosList';
 import AdvertsList from './components/adverts/AdvertsList';
 import DetailsList from './components/details/DetailsList';
 import Navigation from './components/Navigation';
@@ -16,8 +14,6 @@ const App = () => (
     <Navigation />
     <AdvertsList />
     <Bookies />
-    <PaidCombosList />
-    <FreeCombosList />
     <Routes>
       <Route path="/" element={<PredictionsList />} />
       <Route path="/details/:detailId" element={<DetailsList />} />
