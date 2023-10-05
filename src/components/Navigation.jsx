9121197ab4f58ed0728f6d15f4346107a6cb9a2a
Collins Bawa <collinskwamebawa@yahoo.com>
2023-10-05 21:39:25 +0000

    Remove commented code

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,21 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-// import { Link } from 'react-router-dom';
 import Logout from './auth/Logout';
 import logo from '../assets/mrcongrats.jpg';
 
 const Navigation = () => {
   const [menuVisible, setMenuVisible] = useState(false);
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
-  // const dispatch = useDispatch();
 
   const handleToggleMenu = () => {
     setMenuVisible((prevState) => !prevState);
   };
-
-  // const handleLogout = () => {
-  //   dispatch(Logout());
-  // };
 
   useEffect(() => {
     const handleListItemClick = () => {
