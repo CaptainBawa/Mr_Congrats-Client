3ba42f8e7ae35faf2753929cccb4eb35c212def6
Collins Bawa <collinskwamebawa@yahoo.com>
2023-10-07 02:58:02 +0000

    Add note div

--- src/components/SubscriptionNote.jsx
+++ src/components/SubscriptionNote.jsx
@@ -1,17 +1,55 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import Signup from './auth/Signup';
+import Login from './auth/Login';
 
-const SubscriptionNote = () => (
-  <div>
-    <p>
-      <Link to="/signup">Sign up</Link>
-      or
-      <Link to="/login">Login</Link>
-      to access our weekly plan
-      and receive our 20 odds prediction for just 20usd.
-      Join Now!
-    </p>
-  </div>
-);
+const SubscriptionNote = () => {
+  const [showSignup, setShowSignup] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
+
+  const handleSignupClick = () => {
+    setShowSignup(true);
+  };
+
+  const handleLoginClick = () => {
+    setShowLogin(true);
+  };
+
+  return (
+    <div className="note-container">
+      <h2>VIP Tip. Only subscribed users can see this sure tips</h2>
+      <div className="note">
+        <p>
+          <button
+            type="button"
+            onClick={handleSignupClick}
+            style={{
+              cursor: 'pointer', textDecoration: 'underline', border: 'none', background: 'none',
+            }}
+          >
+            Sign up
+          </button>
+          {' '}
+          or
+          {' '}
+          <button
+            type="button"
+            onClick={handleLoginClick}
+            style={{
+              cursor: 'pointer', textDecoration: 'underline', border: 'none', background: 'none',
+            }}
+          >
+            Login
+          </button>
+          {' '}
+          to access our weekly plan
+          and receive our 20 odds prediction for just 20 USD.
+          Join Now!
+        </p>
+        {showSignup && <Signup />}
+        {showLogin && <Login />}
+      </div>
+    </div>
+  );
+};
 
 export default SubscriptionNote;
