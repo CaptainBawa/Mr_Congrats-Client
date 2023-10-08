8327eefa428c5d851ec243297bf3703217758bca
Collins Bawa <collinskwamebawa@yahoo.com>
2023-10-08 15:23:49 +0000

    Import PredictionPanel component

--- src/components/admin/AdminPanel.jsx
+++ src/components/admin/AdminPanel.jsx
@@ -8,6 +8,7 @@ import {
   selectUsersStatus,
   selectUsersError,
 } from '../../redux/slice/usersSlice';
+import PredictionsPanel from './PredictionsPanel';
 
 const AdminPanel = () => {
   const dispatch = useDispatch();
@@ -72,93 +73,99 @@ const AdminPanel = () => {
   }
 
   return (
-    <div className="admin-data">
-      <h1>Admin Panel</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Role</th>
-            <th>Subscribed</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user.id}>
-              <td>{user.id}</td>
-              <td>
-                {editUserId === user.id ? (
-                  <input
-                    type="text"
-                    value={editedUserData.name}
-                    onChange={(e) => setEditedUserData({ ...editedUserData, name: e.target.value })}
-                  />
-                ) : (
-                  user.name
-                )}
-              </td>
-              <td>
-                {editUserId === user.id ? (
-                  <input
-                    type="text"
-                    value={editedUserData.email}
-                    onChange={(e) => setEditedUserData({
-                      ...editedUserData,
-                      email: e.target.value,
-                    })}
-                  />
-                ) : (
-                  user.email
-                )}
-              </td>
-              <td>
-                {editUserId === user.id ? (
-                  <input
-                    type="text"
-                    value={editedUserData.role}
-                    onChange={(e) => setEditedUserData({
-                      ...editedUserData,
-                      role: e.target.value,
-                    })}
-                  />
-                ) : (
-                  user.role
-                )}
-              </td>
-              <td>
-                {editUserId === user.id ? (
-                  <select
-                    value={editedUserData.subscription_status ? 'Yes' : 'No'}
-                    onChange={(e) => handleSubscriptionStatusChange(e.target.value)}
-                  >
-                    <option value="Yes">Yes</option>
-                    <option value="No">No</option>
-                  </select>
-                ) : (
-                  <span>{user.subscription_status ? 'Yes' : 'No'}</span>
-                )}
-              </td>
-              <td>
-                {editUserId === user.id ? (
-                  <>
-                    <button type="button" onClick={handleSaveEdit}>Save</button>
-                    <button id="btn-bg-red" type="button" onClick={handleCancelEdit}>Cancel</button>
-                  </>
-                ) : (
-                  <>
-                    <button type="button" onClick={() => handleEdit(user)}>Edit</button>
-                    <button id="btn-bg-red" type="button" onClick={() => handleDelete(user.id)}>Delete</button>
-                  </>
-                )}
-              </td>
+    <>
+      <div className="admin-data">
+        <h2>All users</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Subscribed</th>
+              <th>Actions</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>
+                  {editUserId === user.id ? (
+                    <input
+                      type="text"
+                      value={editedUserData.name}
+                      onChange={(e) => setEditedUserData({
+                        ...editedUserData,
+                        name: e.target.value,
+                      })}
+                    />
+                  ) : (
+                    user.name
+                  )}
+                </td>
+                <td>
+                  {editUserId === user.id ? (
+                    <input
+                      type="text"
+                      value={editedUserData.email}
+                      onChange={(e) => setEditedUserData({
+                        ...editedUserData,
+                        email: e.target.value,
+                      })}
+                    />
+                  ) : (
+                    user.email
+                  )}
+                </td>
+                <td>
+                  {editUserId === user.id ? (
+                    <input
+                      type="text"
+                      value={editedUserData.role}
+                      onChange={(e) => setEditedUserData({
+                        ...editedUserData,
+                        role: e.target.value,
+                      })}
+                    />
+                  ) : (
+                    user.role
+                  )}
+                </td>
+                <td>
+                  {editUserId === user.id ? (
+                    <select
+                      value={editedUserData.subscription_status ? 'Yes' : 'No'}
+                      onChange={(e) => handleSubscriptionStatusChange(e.target.value)}
+                    >
+                      <option value="Yes">Yes</option>
+                      <option value="No">No</option>
+                    </select>
+                  ) : (
+                    <span>{user.subscription_status ? 'Yes' : 'No'}</span>
+                  )}
+                </td>
+                <td>
+                  {editUserId === user.id ? (
+                    <>
+                      <button type="button" onClick={handleSaveEdit}>Save</button>
+                      <button id="btn-bg-red" type="button" onClick={handleCancelEdit}>Cancel</button>
+                    </>
+                  ) : (
+                    <>
+                      <button type="button" onClick={() => handleEdit(user)}>Edit</button>
+                      <button id="btn-bg-red" type="button" onClick={() => handleDelete(user.id)}>Delete</button>
+                    </>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <PredictionsPanel />
+    </>
   );
 };
 
