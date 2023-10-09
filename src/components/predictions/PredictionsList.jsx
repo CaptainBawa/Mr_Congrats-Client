769a2a3e5293161b11e315600c2b0cfc97323584
Collins Bawa <collinskwamebawa@yahoo.com>
2023-10-09 20:23:10 +0000

    Improve readability and also import note component

--- src/components/predictions/PredictionsList.jsx
+++ src/components/predictions/PredictionsList.jsx
@@ -5,6 +5,7 @@ import { fetchPredictions, selectPredictions } from '../../redux/slice/predictio
 import FreeCombosList from '../free_combos/Free_CombosList';
 import PaidCombosList from '../paid_combos/Paid_CombosList';
 import SubscriptionNote from '../SubscriptionNote';
+import Note from '../Note';
 
 const PredictionsList = () => {
   const dispatch = useDispatch();
@@ -24,91 +25,69 @@ const PredictionsList = () => {
     return `${formattedHours}:${formattedMinutes}${amOrPm}`;
   };
 
+  let content;
+  if (isAuth && subscriptionStatus === true) {
+    content = (
+      <>
+        <PaidCombosList />
+        <FreeCombosList />
+      </>
+    );
+  } else if (isAuth && subscriptionStatus === false) {
+    content = (
+      <>
+        <Note />
+        <FreeCombosList />
+      </>
+    );
+  } else {
+    content = (
+      <>
+        <SubscriptionNote />
+        <FreeCombosList />
+      </>
+    );
+  }
+
   return (
     <div>
-      {isAuth && subscriptionStatus === true ? (
-        <>
-          <PaidCombosList />
-          <FreeCombosList />
-          <div className="free-singles-container">
-            <h2>Free singles</h2>
-            {predictions.map((prediction) => (
-              <div key={prediction.id} className="free-singles">
-                <div className="free-teams">
-                  <div className="team">
-                    <img src={prediction.home_team_logo} alt="home team logo" />
-                    <h3>{prediction.home_team}</h3>
-                  </div>
-                  <div className="league-tip">
-                    <h3>{prediction.league}</h3>
-                    <h3>{prediction.tip}</h3>
-                  </div>
-                  <div className="team">
-                    <img src={prediction.away_team_logo} alt="away team logo" />
-                    <h3>{prediction.away_team}</h3>
-                  </div>
-                </div>
-                <div className="datetime-result">
-                  <h3>
-                    Date:
-                    {prediction.date}
-                  </h3>
-                  <h3>
-                    Time:
-                    {formatTime(prediction.time)}
-                  </h3>
-                  <h3>
-                    Results:
-                    {prediction.result}
-                  </h3>
-                </div>
-                <Link className="h-h" to={`/details/${prediction.id}`}>Head To Head</Link>
+      {content}
+      <div className="free-singles-container">
+        <h2>Free singles</h2>
+        {predictions.map((prediction) => (
+          <div key={prediction.id} className="free-singles">
+            <div className="free-teams">
+              <div className="team">
+                <img src={prediction.home_team_logo} alt="home team logo" />
+                <h3>{prediction.home_team}</h3>
               </div>
-            ))}
-          </div>
-        </>
-      ) : (
-        <>
-          <SubscriptionNote />
-          <FreeCombosList />
-          <div className="free-singles-container">
-            <h2>Free singles</h2>
-            {predictions.map((prediction) => (
-              <div key={prediction.id} className="free-singles">
-                <div className="free-teams">
-                  <div className="team">
-                    <img src={prediction.home_team_logo} alt="home team logo" />
-                    <h3>{prediction.home_team}</h3>
-                  </div>
-                  <div className="league-tip">
-                    <h3>{prediction.league}</h3>
-                    <h3>{prediction.tip}</h3>
-                  </div>
-                  <div className="team">
-                    <img src={prediction.away_team_logo} alt="away team logo" />
-                    <h3>{prediction.away_team}</h3>
-                  </div>
-                </div>
-                <div className="datetime-result">
-                  <h3>
-                    Date:
-                    {prediction.date}
-                  </h3>
-                  <h3>
-                    Time:
-                    {formatTime(prediction.time)}
-                  </h3>
-                  <h3>
-                    Results:
-                    {prediction.result}
-                  </h3>
-                </div>
-                <Link className="h-h" to={`/details/${prediction.id}`}>Head To Head</Link>
+              <div className="league-tip">
+                <h3>{prediction.league}</h3>
+                <h3>{prediction.tip}</h3>
               </div>
-            ))}
+              <div className="team">
+                <img src={prediction.away_team_logo} alt="away team logo" />
+                <h3>{prediction.away_team}</h3>
+              </div>
+            </div>
+            <div className="datetime-result">
+              <h3>
+                Date:
+                {prediction.date}
+              </h3>
+              <h3>
+                Time:
+                {formatTime(prediction.time)}
+              </h3>
+              <h3>
+                Results:
+                {prediction.result}
+              </h3>
+            </div>
+            <Link className="h-h" to={`/details/${prediction.id}`}>Head To Head</Link>
           </div>
-        </>
-      )}
+        ))}
+      </div>
     </div>
   );
 };
