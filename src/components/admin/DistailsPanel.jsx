5f80b5834a653b5c62088701788c0b13f969cac6
Collins Bawa <collinskwamebawa@yahoo.com>
2023-10-13 18:11:28 +0000

    Add logic to create detail

--- src/components/admin/DistailsPanel.jsx
+++ src/components/admin/DistailsPanel.jsx
@@ -1,20 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import {
-  fetchDetails, createDetail, deleteDetail,
-  updateDetail, selectDetails, selectDetailsStatus,
-  selectDetailsError,
-} from '../../redux/slice/detailsSlice';
+import { createDetail } from '../../redux/slice/detailsSlice';
 
 const DetailsPanel = () => {
   const dispatch = useDispatch();
-  const details = useSelector(selectDetails);
-  const status = useSelector(selectDetailsStatus);
-  const error = useSelector(selectDetailsError);
-  const prediction = useSelector((state) => state.predictions?.prediction?.id);
-
-  const [editDetailId, setEditDetailId] = useState(null);
-  const [editedDetailsData, setEditedDetailsData] = useState({});
+  const predictions = useSelector((state) => state.predictions?.predictions);
   const [newDetailsData, setNewDetailsData] = useState({
     date: '',
     home_team: '',
@@ -22,48 +12,16 @@ const DetailsPanel = () => {
     result: '',
     league: '',
     text: '',
-    prediction_id: prediction,
+    prediction_id: '',
   });
-
-  useEffect(() => {
-    dispatch(fetchDetails());
-  }, [dispatch]);
-
-  const handleDetailsEdit = (detail) => {
-    setEditDetailId(detail.id);
-    setEditedDetailsData(detail);
-  };
-
-  const handleDetailsCancelEdit = () => {
-    setEditDetailId(null);
-    setEditedDetailsData({});
-  };
-
-  const handleDetailsSaveEdit = () => {
-    dispatch(updateDetail({ detail: editedDetailsData, id: editDetailId }))
-      .then(() => {
-        setEditDetailId(null);
-        setEditedDetailsData({});
-      })
-      .catch((error) => {
-        throw new Error('Failed to update detail:', error);
-      });
-  };
-
-  const handleDetailsDelete = (detailId) => {
-    dispatch(deleteDetail(detailId))
-      .catch((error) => {
-        throw new Error('Failed to delete detail:', error);
-      });
-  };
 
   const handleDetailsInputChange = (e) => {
     const { name, value } = e.target;
-    setEditedDetailsData({ ...editedDetailsData, [name]: value });
+    setNewDetailsData({ ...newDetailsData, [name]: value });
   };
 
   const handleDetailsAdd = () => {
-    dispatch(createDetail({ detail: newDetailsData }))
+    dispatch(createDetail({ detail: newDetailsData, id: newDetailsData.prediction_id }))
       .then(() => {
         setNewDetailsData({
           date: '',
@@ -72,141 +30,13 @@ const DetailsPanel = () => {
           result: '',
           league: '',
           text: '',
-          prediction_id: prediction,
+          prediction_id: '',
         });
       });
   };
 
-  if (status === 'loading') {
-    return <div>Loading...</div>;
-  }
-
-  if (status === 'failed') {
-    return <div>{error}</div>;
-  }
-
   return (
     <div className="admin-data">
-      <h2>Head to Head</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Date</th>
-            <th>Home Team</th>
-            <th>Away Team</th>
-            <th>Results</th>
-            <th>League</th>
-            <th>Text</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {details.map((detail) => (
-            <tr key={detail.id}>
-              <td>{detail.id}</td>
-              <td>
-                {editDetailId === detail.id ? (
-                  <input
-                    type="date"
-                    value={editedDetailsData.date}
-                    onChange={(e) => setEditedDetailsData({
-                      ...editedDetailsData,
-                      date: e.target.value,
-                    })}
-                  />
-                ) : (
-                  detail.date
-                )}
-              </td>
-              <td>
-                {editDetailId === detail.id ? (
-                  <input
-                    type="text"
-                    value={editedDetailsData.home_team}
-                    onChange={(e) => setEditedDetailsData({
-                      ...editedDetailsData,
-                      home_team: e.target.value,
-                    })}
-                  />
-                ) : (
-                  detail.home_team
-                )}
-              </td>
-              <td>
-                {editDetailId === detail.id ? (
-                  <input
-                    type="text"
-                    name="homeTeam"
-                    value={editedDetailsData.away_team}
-                    onChange={(e) => setEditedDetailsData({
-                      ...editedDetailsData,
-                      away_team: e.target.value,
-                    })}
-                  />
-                ) : (
-                  detail.away_team
-                )}
-              </td>
-              <td>
-                {editDetailId === detail.id ? (
-                  <input
-                    type="text"
-                    value={editedDetailsData.result}
-                    onChange={(e) => setEditedDetailsData({
-                      ...editedDetailsData,
-                      result: e.target.value,
-                    })}
-                  />
-                ) : (
-                  detail.result
-                )}
-              </td>
-              <td>
-                {editDetailId === detail.id ? (
-                  <input
-                    type="text"
-                    value={editedDetailsData.league}
-                    onChange={(e) => setEditedDetailsData({
-                      ...editedDetailsData,
-                      league: e.target.value,
-                    })}
-                  />
-                ) : (
-                  detail.league
-                )}
-              </td>
-              <td>
-                {editDetailId === detail.id ? (
-                  <input
-                    type="text"
-                    value={editedDetailsData.text}
-                    onChange={(e) => setEditedDetailsData({
-                      ...editedDetailsData,
-                      text: e.target.value,
-                    })}
-                  />
-                ) : (
-                  detail.text
-                )}
-              </td>
-              <td>
-                {editDetailId === detail.id ? (
-                  <>
-                    <button type="button" onClick={handleDetailsSaveEdit}>Save</button>
-                    <button id="btn-bg-red" type="button" onClick={handleDetailsCancelEdit}>Cancel</button>
-                  </>
-                ) : (
-                  <>
-                    <button type="button" onClick={() => handleDetailsEdit(detail)}>Edit</button>
-                    <button id="btn-bg-red" type="button" onClick={() => handleDetailsDelete(detail.id)}>Delete</button>
-                  </>
-                )}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
       <div className="add-data">
         <h2>Add Head to Head</h2>
         <input
@@ -244,6 +74,20 @@ const DetailsPanel = () => {
           value={newDetailsData.league}
           onChange={handleDetailsInputChange}
         />
+        <select
+          name="prediction_id"
+          value={newDetailsData.prediction_id}
+          onChange={handleDetailsInputChange}
+        >
+          <option value="">Select Prediction</option>
+          {predictions.map((prediction) => (
+            <option key={prediction.id} value={prediction.id}>
+              {prediction.home_team}
+              {' '}
+              {prediction.away_team}
+            </option>
+          ))}
+        </select>
         <textarea
           name="text"
           placeholder="text"
